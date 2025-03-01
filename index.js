c132c726e7b69723b78af11f07395b74ed8d49c4
thuynh29 <thuynh29@huskers.unl.edu>
2025-03-01 14:46:17 -0600

    Testing

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-// const myModule = require('./my-module');
 const express = require('express');
 const app = express(); 
 const fs = require('fs');
-// const {EventEmitter} = require('events');
-// const eventEmitter = new EventEmitter();
 
-// eventEmitter.on('lunch', () => {
-// 	console.log('something');
-// })
+let temp;
+let it;
 
-// Calling it   
-//eventEmitter.emit('lunch')
+app.use(express.urlencoded({extended:true}));
 
-// const { readFile } = require('fs').promises;
-
-// async function hello() {
-//     const file = await readFile('./text.txt', 'utf-8');
-//     console.log(file);
-// }
-
-app.get('/', (request, response) => {
+// Local host connection HTML
+app.get('/', (req, res) => {
 
     fs.readFile('./home.html', 'utf8', (err, html) => {
 
@@ -27,12 +16,40 @@ app.get('/', (request, response) => {
             response.status(500).send('Unavailable')
         }
 
-        response.send(html)
+        //response.send(html)
+        res.send(`
+            <form action="/submit" method="POST">
+              <label for="temperature">Temperature:</label>
+              <input type="text" id="temperature" name="temperature" required>
+              <label for="item">Item:</label>
+              <input type="text" id="item" name="item" required>
+              <button type="submit">Submit</button>
+            </form>
+          `);
 
     })
 })
+
+// User input
+app.post('/submit', (req, res) => {
+    const { temperature, item } = req.body;
+  
+    if (!temperature || !item) {
+      return res.status(400).send('Please try again.');
+    }
+
+    console.log(`Received temperature: ${temperature}, item: ${item}`);
+  
+    res.send(`Received ${temperature} and ${item}`);
+
+    
+  });
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+function store_earth_data(year, integer) {
+    
+}
