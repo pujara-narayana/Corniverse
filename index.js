611b5b80b45a44cc3f4be4e9a52ea42a7c1f3690
thuynh29 <thuynh29@huskers.unl.edu>
2025-03-01 13:23:05 -0600

    Add package json and set up basic module

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const myModule = require('./my-module');
 const {EventEmitter} = require('events');
 const eventEmitter = new EventEmitter();
 
 eventEmitter.on('lunch', () => {
-	console.log('something')
+	console.log('something');
 })
 
 // Calling it
-eventEmitter.emit('lunch')
+//eventEmitter.emit('lunch')
+
+const { readFile } = require('fs').promises;
+
+async function hello() {
+    const file = await readFile('./text.txt', 'utf-8');
+    console.log(file);
+}
+
+console.log(myModule)
